0f6eb77fcf0e73c80561655c4bdc9ad9f58da124
pshort911 <pshort911@gmail.com>
2019-01-10 15:58:47 -0500

    Add files via upload

---
+++ Controllers/ComicBookDetailsController.cs
@@ -0,0 +1,130 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicBookRepository.Controllers
+{
+    public class ComicBookDetailsController : Controller
+    {
+        #region Private Fields
+
+        private readonly ComicBookRepositoryContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private string _userId;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public ComicBookDetailsController(ComicBookRepositoryContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        // GET: ComicBookDetails/Create
+        public IActionResult Create() => View();
+
+        // POST: ComicBookDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,OwnerId,Title,SpecialIssue,IssueNum,IssueName,Grade,Rating,Description,Own,Want")]
+            ComicBookDetails comicBookDetails)
+        {
+            if (!ModelState.IsValid) return View(comicBookDetails);
+            comicBookDetails.OwnerId = _userId;
+            _context.Add(comicBookDetails);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index), new { comicBookDetails.Title.Id });
+
+        }
+
+        // GET: ComicBookDetails/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null) return NotFound();
+
+            var comicBookDetails = await _context.ComicBookDetails
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comicBookDetails == null) return NotFound();
+
+            return View(comicBookDetails);
+        }
+
+        // POST: ComicBookDetails/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var comicBookDetails = await _context.ComicBookDetails.FindAsync(id);
+            _context.ComicBookDetails.Remove(comicBookDetails);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index), new { comicBookDetails.Title.Id });
+        }
+
+        // GET: ComicBookDetails/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null) return NotFound();
+
+            var comicBookDetails = await _context.ComicBookDetails.FindAsync(id);
+            if (comicBookDetails == null) return NotFound();
+            return View(comicBookDetails);
+        }
+
+        // POST: ComicBookDetails/Edit/5 To protect from overposting attacks, please enable the specific properties you want to bind to, for more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,OwnerId,Title,SpecialIssue,IssueNum,IssueName,Grade,Rating,Description,Own,Want")]
+            ComicBookDetails comicBookDetails)
+        {
+            if (id != comicBookDetails.Id) return NotFound();
+
+            if (!ModelState.IsValid) return View(comicBookDetails);
+
+            try
+            {
+                comicBookDetails.OwnerId = _userId;
+                _context.Update(comicBookDetails);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ComicBookDetailsExists(comicBookDetails.Id)) return NotFound();
+
+                throw;
+            }
+
+            return RedirectToAction(nameof(Index), new { comicBookDetails.Title.Id });
+        }
+
+        //GET: ComicBookDetails/asdf
+        public async Task<IActionResult> Index(long? id)
+        {
+            var user = await GetCurrentUserAsync();
+            _userId = user?.Id;
+            if (_userId == null) return NotFound();
+
+            var comicBookDetails = id == null ? _context.ComicBookDetails.Where(m => m.OwnerId == _userId) : _context.ComicBookDetails.Where(m => m.OwnerId == _userId && m.TitleId == id).Include(t => t.Title);
+            return View(comicBookDetails);
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private bool ComicBookDetailsExists(long id) => _context.ComicBookDetails.Any(e => e.Id == id);
+
+        private Task<IdentityUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        #endregion Private Methods
+    }
+}
---
+++ Models/ComicBookRepositoryContext.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ComicBookRepository
+{
+    public partial class ComicBookRepositoryContext : DbContext
+    {
+        public ComicBookRepositoryContext()
+        {
+        }
+
+        public ComicBookRepositoryContext(DbContextOptions<ComicBookRepositoryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ComicBookDetails> ComicBookDetails { get; set; }
+        public virtual DbSet<ComicBookTitle> ComicBookTitle { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+
+            modelBuilder.Entity<ComicBookDetails>(entity =>
+            {
+                entity.HasIndex(e => e.Own)
+                    .HasName("ComicBookDetails_IX_ComicBookDetails");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Description).HasColumnType("ntext");
+
+                entity.Property(e => e.Grade).HasColumnType("nvarchar (20)");
+
+                entity.Property(e => e.IssueName).HasColumnType("nvarchar (100)");
+
+                entity.Property(e => e.IssueNum).HasColumnType("int");
+
+                entity.Property(e => e.Own)
+                    .IsRequired()
+                    .HasColumnType("BOOLEAN");
+
+                entity.Property(e => e.OwnerId).HasColumnType("nvarchar (450)");
+
+                entity.Property(e => e.Rating).HasColumnType("nvarchar (50)");
+
+                entity.Property(e => e.SpecialIssue)
+                    .IsRequired()
+                    .HasColumnType("BOOLEAN");
+
+                entity.Property(e => e.TitleId).HasColumnName("TitleID");
+
+                entity.Property(e => e.Want)
+                    .IsRequired()
+                    .HasColumnType("BOOLEAN");
+
+                entity.HasOne(d => d.Title)
+                    .WithMany(p => p.ComicBookDetails)
+                    .HasForeignKey(d => d.TitleId);
+            });
+
+            modelBuilder.Entity<ComicBookTitle>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.FirstIssue).HasColumnType("int");
+
+                entity.Property(e => e.LastIssue).HasColumnType("int");
+
+                entity.Property(e => e.LimitedSeries)
+                    .IsRequired()
+                    .HasColumnType("BOOLEAN");
+
+                entity.Property(e => e.NumIssues).HasColumnType("int");
+
+                entity.Property(e => e.NumSpIssues).HasColumnType("int");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnType("nvarchar (100)");
+            });
+        }
+    }
+}
